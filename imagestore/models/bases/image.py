4a15e88c2816444f3748357e737af82a0a5b75c8
Russ Ferriday <russf@topia.com>
2014-06-15 21:53:49 +0100

    make dates from EXIF timestamps have the current timezone

--- imagestore/models/bases/image.py
+++ imagestore/models/bases/image.py
@@ -10,6 +10,7 @@ from django.db import models
 from django.db.models import permalink
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
+from django.utils import timezone
 from django.contrib.auth.models import Permission
 from django.db.models.signals import post_save
 from django.contrib.contenttypes.models import ContentType
@@ -131,11 +132,13 @@ class BaseImage(models.Model):
 
     def raw_exif_datetime(self):
         """
-        Get the raw exif date, returning None if on a KeyError or parsing problem
+        Get the raw exif date with TZ, returning None if on a KeyError or parsing problem
         """
         try:
             exifdate = self.exif['Image DateTime']
             dt = datetime.datetime.strptime(exifdate, '%Y:%m:%d %H:%M:%S')
+            if timezone.is_naive(dt):
+                dt = timezone.make_aware(timezone.get_current_timezone())
             return dt
         except Exception, ex:
             logger.exception("raw_exif_datetime")
